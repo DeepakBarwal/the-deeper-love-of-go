ae5ddb0999600466a08e74144d3e88263026520d
DeepakBarwal <deepakbarwal98@gmail.com>
2026-09-01 21:23:21 +0530

    go mod init

--- main.go
+++ main.go
@@ -9,24 +9,10 @@ type Book struct {
 }
 
 func main() {
-	TestBookToString_FormatsBookInfoAsString()
 	fmt.Println("It's all good!")
 }
 
 func BookToString(book Book) string {
 	return fmt.Sprintf("%v by %v - %v copies",
 		book.Title, book.Author, book.Copies)
-}
-
-func TestBookToString_FormatsBookInfoAsString() {
-	input := Book{
-		Title:  "Sea Room",
-		Author: "Adam Nicolson",
-		Copies: 2,
-	}
-	want := "Sea Room by Adam Nicolson - 2 copies"
-	got := BookToString(input)
-	if want != got {
-		panic("BookToString: wrong result")
-	}
 }
