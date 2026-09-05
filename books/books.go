3d606acddd61a19a13698bf94810102f2c5d5dfa
DeepakBarwal <deepakbarwal98@gmail.com>
2026-09-05 18:34:28 +0530

    add: GetBook

--- books/books.go
+++ books/books.go
@@ -8,6 +8,7 @@ type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ID     string
 }
 
 func BookToString(book Book) string {
@@ -20,14 +21,25 @@ var catalog = []Book{
 		Title:  "In the Company of Cheerful Ladies",
 		Author: "Alexander McCall Smith",
 		Copies: 1,
+		ID:     "abc",
 	},
 	{
 		Title:  "White Heat",
 		Author: "Dominic Sandbrook",
 		Copies: 2,
+		ID:     "xyz",
 	},
 }
 
 func GetAllBooks() []Book {
 	return catalog
+}
+
+func GetBook(ID string) (Book, bool) {
+	for _, book := range catalog {
+		if book.ID == ID {
+			return book, true
+		}
+	}
+	return Book{}, false
 }
