b47cfff5e3765c6b0c92098373db6ea1f840ec76
DeepakBarwal <deepakbarwal98@gmail.com>
2026-09-12 15:12:30 +0530

    feat: cmd/server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"books/books"
 	"fmt"
-	"net/http"
+	"os"
 )
 
 func main() {
-	http.ListenAndServe(":3000", http.HandlerFunc(hello))
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: server <CATALOG FILE>")
+		return
+	}
+	catalog, err := books.OpenCatalog(os.Args[1])
+	if err != nil {
+		fmt.Printf("opening catalog: %v\n", err)
+		return
+	}
+	err = books.ListenAndServe(":3000", catalog)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
